0b1bd02f4cfbfbc1fdc2bc7bf4a1b8185b3035fd
k2k <kadonna@wonderkul.com>
2019-08-18 21:51:26 -0500

    Still working on posting user's survey results to the friends array.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,111 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends who have completed a survey in an array.
+// Will add some "friends" to get started.
+
 //application's data as an array of objects. Each of these objects should roughly follow the format below;
-/*
-        json
+
+var friendsArray = [
         {
-        "name":"Ahmed",
+        "name":"Rachel",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
-            5,
-            1,
             4,
-            4,
-            5,
-            1,
             2,
-            5,
+            2,
             4,
-            1
+            4,
+            2,
+            1,
+            3,
+            5,
+            4
             ]
-        }
-*/
+        },
+        {
+          "name":"Monica",
+          "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+          "scores":[
+              3,
+              4,
+              3,
+              4,
+              2,
+              1,
+              3,
+              3,
+              5,
+              4
+              ]
+          },
+          {
+            "name":"Phoebe",
+            "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores":[
+                5,
+                1,
+                3,
+                1,
+                3,
+                1,
+                3,
+                1,
+                5,
+                4
+                ]
+            }, 
+          {
+              "name":"Joey",
+              "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+              "scores":[
+                  4,
+                  2,
+                  4,
+                  3,
+                  4,
+                  3,
+                  2,
+                  3,
+                  5,
+                  3
+                  ]
+              },
+              {
+                "name":"Chandler",
+                "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+                "scores":[
+                    3,
+                    4,
+                    4,
+                    3,
+                    4,
+                    3,
+                    4,
+                    4,
+                    5,
+                    2
+                    ]
+                },
+                {
+                  "name":"Ross",
+                  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+                  "scores":[
+                      2,
+                      4,
+                      3,
+                      4,
+                      3,
+                      3,
+                      4,
+                      5,
+                      5,
+                      3
+                      ]
+                  }         
+      ]
 
+
+module.exports = friendsArray;
 /*
 Determine the user's most compatible friend using the following as a guide:
 
--- server.js
+++ server.js
@@ -14,7 +14,11 @@ var PORT = process.env.PORT || 3005;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("apiRoutes")(app);
+require("htmlRoutes")(app);
+
 //connect to server
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
